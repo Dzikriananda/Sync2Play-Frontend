76f87d73c698f09b7c9a35cc6bf0d9cf004a1075
Dzikriananda <dzikry.785@gmail.com>
2025-10-08 18:22:02 +0700

    fix delay

--- src/components/AudioPlayerComponent.jsx
+++ src/components/AudioPlayerComponent.jsx
@@ -36,7 +36,7 @@
       
         console.log(`before Audio will play in ${delay.toFixed(0)} ms`);
         if(isDesktopOS()) {
-          delay += 300;
+          delay += 400;
         }
         console.log(`after Audio will play in ${delay.toFixed(0)} ms`);
 
