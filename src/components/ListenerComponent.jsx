0e3b18959a7b7eba8ff25adb1260a2a9336c6d4f
Dzikriananda <dzikry.785@gmail.com>
2025-10-10 23:23:18 +0700

    fix attempt iphone 15

--- src/components/ListenerComponent.jsx
+++ src/components/ListenerComponent.jsx
@@ -11,7 +11,7 @@ export default function ListenerComponent() {
   function onMediaDownloaded(code, file) {
     setCode(code);
     // setFile(file);
-    console.log('before parsed');
+    console.log('before parsed' + file);
     const parsedFile = new File([file], `andai aku bisa.mp3`,  { type: 'audio/mpeg' } );
     console.log(parsedFile);
     setFile(parsedFile);
