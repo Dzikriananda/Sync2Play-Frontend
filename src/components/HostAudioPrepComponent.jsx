da07f0e2e7fce161c86c2d7ea501084bdce780b8
Dzikriananda <dzikry.785@gmail.com>
2025-10-12 17:01:04 +0700

    fix card for mobile

--- src/components/HostAudioPrepComponent.jsx
+++ src/components/HostAudioPrepComponent.jsx
@@ -229,14 +229,13 @@ function HostAudioPrepComponent({callBackWhenMediaReady,callBackWhenUploadFinish
       {
         (youtubeData != null) ? 
         <div>
-            <div className=" mt-4 mb-4 p-3 border border-gray-200 rounded-lg flex items-center space-x-3 bg-gray-50 shadow-inner mb-6">
-              <img
+            <div className="mt-4p-3 border border-gray-200 rounded-lg flex flex-col sm:flex-row items-start sm:items-center space-y-3 sm:space-y-0 sm:space-x-3 bg-gray-50 shadow-inner mb-6">              <img
                 src={youtubeData.thumbnailUrl}
                 alt="Cover Art"
                 className="h-36 w-auto max-w-full sm:max-w-64 rounded-md object-cover"
               />
               <div className="flex-1 min-w-0">
-                <p className="text-sm font-semibold text-gray-800 ">
+                <p className="text-sm font-semibold text-gray-800 break-words">
                   {youtubeData.title}
                 </p>
                 <p className="text-xs text-gray-500 truncate">
